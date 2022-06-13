127d3ade1763129fcad7d7f7d3ad89d4ff7f116c
SeryoungPark <76822758+SeryoungPark@users.noreply.github.com>
2022-06-14 01:03:55 +0900

    Update not.js

--- not.js
+++ not.js
@@ -1,7 +1,7 @@
 
 var arr = ["person1.html",
 "person2.html", "person3.html",
-"person4.html","person5.html","person6.html","person7.html","person8.html"."person9.html","person10.html","person11.html","person12.html"];
+"person4.html","person5.html","person6.html","person7.html","person8.html","person9.html","person10.html","person11.html","person12.html"];
 
 function randSite(){
         location.href = "" + arr[Math.floor (Math.random()*arr.length)];
